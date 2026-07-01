4e1426e8c0cd6dd823bc62c4b087ae8dc68c5998
mohamed saleh <mohamad.asaleeh@gmail.com>
2026-07-01 23:13:27 +0300

    finished issue 3

--- kids/urls.js
+++ kids/urls.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { addKid, getKidsOf, callKid, getAllKids, confirmKid } from "./kids.js";
+import { addKid, getKidsOf, callKid } from "./kids.js";
 import {
   validateAddingKid,
   validateGetKidsOf,
   validateCallKid,
-  validateConfirmKid,
 } from "./validators.js";
 
 export const router = express.Router();
@@ -14,7 +13,3 @@ router.post("/", validateAddingKid, addKid);
 router.get("/:id", validateGetKidsOf, getKidsOf);
 
 router.post("/:id/call", validateCallKid, callKid);
-
-router.get("/admin/all", getAllKids);
-
-router.post("/:id/confirm", validateConfirmKid, confirmKid); // issue #5
